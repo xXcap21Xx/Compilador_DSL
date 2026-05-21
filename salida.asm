c21caad2e804e106c9d188b73231e668cd5b4659
Carlos Alberto Perez <80500027+xXcap21Xx@users.noreply.github.com>
2026-05-20 17:43:46 -0700

    Add data structures and visualization ops
    Introduce HEAP and runtime structures (miPila, miCola, miLista) and temporaries T1..T9. Implement stack (push/pop/top), queue (enqueue/dequeue/front), and linked-list (insert front/end, size) logic and error checks in salida.asm with corresponding prints. Add graphical data and rendering routines in salida_grafico.asm (GRAFICAR_TODO plus GRAFICAR_PILA_miPila, GRAFICAR_COLA_miCola, GRAFICAR_LISTA_miLista) to visualize the structures, and update salida_visualizacion.txt to reflect the generated quadruples for these ops.

--- salida.asm
+++ salida.asm
@@ -11,7 +11,25 @@
     newline db 0Dh, 0Ah, '$'
     msg_error db 'Error en la operacion', 0Dh, 0Ah, '$'
 
-    msg_1 db 'Código Intermedio OK', '$'
+    HEAP dw 1000 dup(0)
+    HEAP_PTR dw 2
+    miPila dw 100 dup(0)
+    miPila_top dw 0
+    miCola dw 50 dup(0)
+    miCola_front dw 0
+    miCola_rear dw 0
+    miCola_count dw 0
+    miLista_head dw 0
+    miLista_tail dw 0
+    T1 dw 0
+    T2 dw 0
+    T3 dw 0
+    T4 dw 0
+    T5 dw 0
+    T6 dw 0
+    T7 dw 0
+    T8 dw 0
+    T9 dw 0
 
 .code
 main proc
@@ -20,24 +38,315 @@ main proc
     mov dx, offset titulo
     call print_string
 
-    ; PRINT 15
-    mov ax, 15
+    ; CREAR PILA miPila TAMANO 100
+    ; APILAR 10 EN miPila
+    mov ax, 10
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov miPila[bx], ax
+    inc word ptr [miPila_top]
+    ; APILAR 20 EN miPila
+    mov ax, 20
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov miPila[bx], ax
+    inc word ptr [miPila_top]
+    ; APILAR 30 EN miPila
+    mov ax, 30
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov miPila[bx], ax
+    inc word ptr [miPila_top]
+    ; TOPE EN miPila
+    cmp word ptr [miPila_top], 0
+    je ASM_L1
+    mov bx, [miPila_top]
+    dec bx
+    shl bx, 1
+    mov ax, miPila[bx]
+    mov [T1], ax
+    jmp ASM_L2
+ASM_L1:
+    mov word ptr [T1], 0
+ASM_L2:
+    ; PRINT T1
+    mov ax, [T1]
     call print_num
     mov dx, offset newline
     call print_string
-    ; PRINT 5
+    ; VACIA
+    mov word ptr [T2], 0
+    cmp word ptr [miPila_top], 0
+    je ASM_L3
+    jmp ASM_L4
+ASM_L3:
+    mov word ptr [T2], 1
+ASM_L4:
+    ; IF_FALSE T2 GOTO L1
+    mov ax, [T2]
+    cmp ax, 0
+    je L1
+    ; ERROR: Estructura vacía 
+    mov dx, offset msg_error
+    call print_string
+L1:
+    ; DESAPILAR EN miPila
+    cmp word ptr [miPila_top], 0
+    je ASM_L5
+    dec word ptr [miPila_top]
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov word ptr miPila[bx], 0
+ASM_L5:
+    ; TOPE EN miPila
+    cmp word ptr [miPila_top], 0
+    je ASM_L6
+    mov bx, [miPila_top]
+    dec bx
+    shl bx, 1
+    mov ax, miPila[bx]
+    mov [T3], ax
+    jmp ASM_L7
+ASM_L6:
+    mov word ptr [T3], 0
+ASM_L7:
+    ; PRINT T3
+    mov ax, [T3]
+    call print_num
+    mov dx, offset newline
+    call print_string
+    ; VACIA
+    mov word ptr [T4], 0
+    cmp word ptr [miPila_top], 0
+    je ASM_L8
+    jmp ASM_L9
+ASM_L8:
+    mov word ptr [T4], 1
+ASM_L9:
+    ; IF_FALSE T4 GOTO L2
+    mov ax, [T4]
+    cmp ax, 0
+    je L2
+    ; ERROR: Estructura vacía 
+    mov dx, offset msg_error
+    call print_string
+L2:
+    ; DESAPILAR EN miPila
+    cmp word ptr [miPila_top], 0
+    je ASM_L10
+    dec word ptr [miPila_top]
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov word ptr miPila[bx], 0
+ASM_L10:
+    ; TOPE EN miPila
+    cmp word ptr [miPila_top], 0
+    je ASM_L11
+    mov bx, [miPila_top]
+    dec bx
+    shl bx, 1
+    mov ax, miPila[bx]
+    mov [T5], ax
+    jmp ASM_L12
+ASM_L11:
+    mov word ptr [T5], 0
+ASM_L12:
+    ; PRINT T5
+    mov ax, [T5]
+    call print_num
+    mov dx, offset newline
+    call print_string
+    ; CREAR COLA miCola TAMANO 50
+    ; ENCOLAR 100 EN miCola
+    cmp word ptr [miCola_count], 50
+    jge ASM_L14
+    mov ax, 100
+    mov bx, [miCola_rear]
+    shl bx, 1
+    mov miCola[bx], ax
+    inc word ptr [miCola_rear]
+    cmp word ptr [miCola_rear], 50
+    jl ASM_L13
+    mov word ptr [miCola_rear], 0
+ASM_L13:
+    inc word ptr [miCola_count]
+ASM_L14:
+    ; ENCOLAR 200 EN miCola
+    cmp word ptr [miCola_count], 50
+    jge ASM_L16
+    mov ax, 200
+    mov bx, [miCola_rear]
+    shl bx, 1
+    mov miCola[bx], ax
+    inc word ptr [miCola_rear]
+    cmp word ptr [miCola_rear], 50
+    jl ASM_L15
+    mov word ptr [miCola_rear], 0
+ASM_L15:
+    inc word ptr [miCola_count]
+ASM_L16:
+    ; ENCOLAR 300 EN miCola
+    cmp word ptr [miCola_count], 50
+    jge ASM_L18
+    mov ax, 300
+    mov bx, [miCola_rear]
+    shl bx, 1
+    mov miCola[bx], ax
+    inc word ptr [miCola_rear]
+    cmp word ptr [miCola_rear], 50
+    jl ASM_L17
+    mov word ptr [miCola_rear], 0
+ASM_L17:
+    inc word ptr [miCola_count]
+ASM_L18:
+    ; FRENTE EN miCola
+    cmp word ptr [miCola_count], 0
+    je ASM_L19
+    mov bx, [miCola_front]
+    shl bx, 1
+    mov ax, miCola[bx]
+    mov [T6], ax
+    jmp ASM_L20
+ASM_L19:
+    mov word ptr [T6], 0
+ASM_L20:
+    ; PRINT T6
+    mov ax, [T6]
+    call print_num
+    mov dx, offset newline
+    call print_string
+    ; VACIA
+    mov word ptr [T7], 0
+    cmp word ptr [miCola_count], 0
+    je ASM_L21
+    jmp ASM_L22
+ASM_L21:
+    mov word ptr [T7], 1
+ASM_L22:
+    ; IF_FALSE T7 GOTO L3
+    mov ax, [T7]
+    cmp ax, 0
+    je L3
+    ; ERROR: Estructura vacía 
+    mov dx, offset msg_error
+    call print_string
+L3:
+    ; DESENCOLAR EN miCola
+    cmp word ptr [miCola_count], 0
+    je ASM_L24
+    mov bx, [miCola_front]
+    shl bx, 1
+    mov word ptr miCola[bx], 0
+    inc word ptr [miCola_front]
+    cmp word ptr [miCola_front], 50
+    jl ASM_L23
+    mov word ptr [miCola_front], 0
+ASM_L23:
+    dec word ptr [miCola_count]
+ASM_L24:
+    ; FRENTE EN miCola
+    cmp word ptr [miCola_count], 0
+    je ASM_L25
+    mov bx, [miCola_front]
+    shl bx, 1
+    mov ax, miCola[bx]
+    mov [T8], ax
+    jmp ASM_L26
+ASM_L25:
+    mov word ptr [T8], 0
+ASM_L26:
+    ; PRINT T8
+    mov ax, [T8]
+    call print_num
+    mov dx, offset newline
+    call print_string
+    ; CREAR LISTA miLista
+    ; INSERTAR_FINAL 5 EN miLista (nodo HEAP: valor, sig)
     mov ax, 5
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne ASM_L27
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp ASM_L28
+ASM_L27:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+ASM_L28:
+    ; INSERTAR_FINAL 10 EN miLista (nodo HEAP: valor, sig)
+    mov ax, 10
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne ASM_L29
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp ASM_L30
+ASM_L29:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+ASM_L30:
+    ; INSERTAR_FINAL 15 EN miLista (nodo HEAP: valor, sig)
+    mov ax, 15
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne ASM_L31
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp ASM_L32
+ASM_L31:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+ASM_L32:
+    ; INSERTAR_INICIO 1 EN miLista (nodo HEAP: valor, sig)
+    mov ax, 1
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne ASM_L33
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp ASM_L34
+ASM_L33:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+ASM_L34:
+    ; TAMANO EN miLista recorriendo punteros HEAP
+    mov bx, [miLista_head]
+    xor cx, cx
+    mov word ptr [T9], 0
+ASM_L35:
+    cmp bx, 0
+    je ASM_L36
+    inc cx
+    mov ax, HEAP[bx]
+    mov [T9], ax
+    mov bx, HEAP[bx+2]
+    jmp ASM_L35
+ASM_L36:
+    mov [T9], cx
+    ; PRINT T9
+    mov ax, [T9]
     call print_num
     mov dx, offset newline
     call print_string
-    ; PRINT 50
-    mov ax, 50
+    ; PRINT 999
+    mov ax, 999
     call print_num
-    mov dx, offset newline
-    call print_string
-    ; PRINT "Código Intermedio OK"
-    mov dx, offset msg_1
-    call print_string
     mov dx, offset newline
     call print_string
 
--- salida_grafico.asm
+++ salida_grafico.asm
@@ -9,6 +9,8 @@
 .data
     titulo db 'DSL - VISUALIZACION GRAFICA', 0
 
+    HEAP dw 1000 dup(0)
+    HEAP_PTR dw 2
     gfx_i dw 0
     gfx_valor dw 0
     rect_x dw 0
@@ -16,7 +18,20 @@
     rect_w dw 0
     rect_h dw 0
     rect_color db 0
-    "Código Intermedio OK" dw 0
+    miPila dw 100 dup(0)
+    miPila_top dw 0
+    miCola dw 50 dup(0)
+    miCola_front dw 0
+    miCola_rear dw 0
+    miCola_count dw 0
+    miLista_head dw 0
+    miLista_tail dw 0
+    T1 dw 0
+    T3 dw 0
+    T5 dw 0
+    T6 dw 0
+    T8 dw 0
+    T9 dw 0
 
 .code
 main proc
@@ -27,17 +42,225 @@ main proc
     mov ax, 0013h
     int 10h
 
-    ; MOSTRAR 15 en modo grafico
-    mov ax, 15
+    ; CREAR PILA miPila TAMANO 100
+    call GRAFICAR_TODO
+    ; APILAR 10 EN miPila
+    cmp word ptr [miPila_top], 100
+    jge GFX_L1
+    mov ax, 10
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov miPila[bx], ax
+    inc word ptr [miPila_top]
+GFX_L1:
+    call GRAFICAR_TODO
+    ; APILAR 20 EN miPila
+    cmp word ptr [miPila_top], 100
+    jge GFX_L2
+    mov ax, 20
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov miPila[bx], ax
+    inc word ptr [miPila_top]
+GFX_L2:
+    call GRAFICAR_TODO
+    ; APILAR 30 EN miPila
+    cmp word ptr [miPila_top], 100
+    jge GFX_L3
+    mov ax, 30
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov miPila[bx], ax
+    inc word ptr [miPila_top]
+GFX_L3:
+    call GRAFICAR_TODO
+    ; Operacion grafica pendiente: TOPE miPila  -> T1
+    ; MOSTRAR T1 en modo grafico
+    mov ax, [T1]
     call PRINT_NUM_GRAFICO
-    ; MOSTRAR 5 en modo grafico
+    ; Operacion grafica pendiente: VACIA miPila  -> T2
+    ; Operacion grafica pendiente: IF_FALSE T2 GOTO -> L1
+    ; Operacion grafica pendiente: ERROR Estructura vacía  -> 
+    ; Operacion grafica pendiente: ETIQUETA   -> L1
+    ; DESAPILAR EN miPila
+    cmp word ptr [miPila_top], 0
+    je GFX_L4
+    dec word ptr [miPila_top]
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov word ptr miPila[bx], 0
+GFX_L4:
+    call GRAFICAR_TODO
+    ; Operacion grafica pendiente: TOPE miPila  -> T3
+    ; MOSTRAR T3 en modo grafico
+    mov ax, [T3]
+    call PRINT_NUM_GRAFICO
+    ; Operacion grafica pendiente: VACIA miPila  -> T4
+    ; Operacion grafica pendiente: IF_FALSE T4 GOTO -> L2
+    ; Operacion grafica pendiente: ERROR Estructura vacía  -> 
+    ; Operacion grafica pendiente: ETIQUETA   -> L2
+    ; DESAPILAR EN miPila
+    cmp word ptr [miPila_top], 0
+    je GFX_L5
+    dec word ptr [miPila_top]
+    mov bx, [miPila_top]
+    shl bx, 1
+    mov word ptr miPila[bx], 0
+GFX_L5:
+    call GRAFICAR_TODO
+    ; Operacion grafica pendiente: TOPE miPila  -> T5
+    ; MOSTRAR T5 en modo grafico
+    mov ax, [T5]
+    call PRINT_NUM_GRAFICO
+    ; CREAR COLA miCola TAMANO 50
+    call GRAFICAR_TODO
+    ; ENCOLAR 100 EN miCola
+    cmp word ptr [miCola_count], 50
+    jge GFX_L7
+    mov ax, 100
+    mov bx, [miCola_rear]
+    shl bx, 1
+    mov miCola[bx], ax
+    inc word ptr [miCola_rear]
+    cmp word ptr [miCola_rear], 50
+    jl GFX_L6
+    mov word ptr [miCola_rear], 0
+GFX_L6:
+    inc word ptr [miCola_count]
+GFX_L7:
+    call GRAFICAR_TODO
+    ; ENCOLAR 200 EN miCola
+    cmp word ptr [miCola_count], 50
+    jge GFX_L9
+    mov ax, 200
+    mov bx, [miCola_rear]
+    shl bx, 1
+    mov miCola[bx], ax
+    inc word ptr [miCola_rear]
+    cmp word ptr [miCola_rear], 50
+    jl GFX_L8
+    mov word ptr [miCola_rear], 0
+GFX_L8:
+    inc word ptr [miCola_count]
+GFX_L9:
+    call GRAFICAR_TODO
+    ; ENCOLAR 300 EN miCola
+    cmp word ptr [miCola_count], 50
+    jge GFX_L11
+    mov ax, 300
+    mov bx, [miCola_rear]
+    shl bx, 1
+    mov miCola[bx], ax
+    inc word ptr [miCola_rear]
+    cmp word ptr [miCola_rear], 50
+    jl GFX_L10
+    mov word ptr [miCola_rear], 0
+GFX_L10:
+    inc word ptr [miCola_count]
+GFX_L11:
+    call GRAFICAR_TODO
+    ; Operacion grafica pendiente: FRENTE miCola  -> T6
+    ; MOSTRAR T6 en modo grafico
+    mov ax, [T6]
+    call PRINT_NUM_GRAFICO
+    ; Operacion grafica pendiente: VACIA miCola  -> T7
+    ; Operacion grafica pendiente: IF_FALSE T7 GOTO -> L3
+    ; Operacion grafica pendiente: ERROR Estructura vacía  -> 
+    ; Operacion grafica pendiente: ETIQUETA   -> L3
+    ; DESENCOLAR EN miCola
+    cmp word ptr [miCola_count], 0
+    je GFX_L13
+    mov bx, [miCola_front]
+    shl bx, 1
+    mov word ptr miCola[bx], 0
+    inc word ptr [miCola_front]
+    cmp word ptr [miCola_front], 50
+    jl GFX_L12
+    mov word ptr [miCola_front], 0
+GFX_L12:
+    dec word ptr [miCola_count]
+GFX_L13:
+    call GRAFICAR_TODO
+    ; Operacion grafica pendiente: FRENTE miCola  -> T8
+    ; MOSTRAR T8 en modo grafico
+    mov ax, [T8]
+    call PRINT_NUM_GRAFICO
+    ; CREAR LISTA miLista TAMANO 100
+    call GRAFICAR_TODO
+    ; INSERTAR_FINAL 5 EN miLista
     mov ax, 5
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne GFX_L14
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp GFX_L15
+GFX_L14:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+GFX_L15:
+    call GRAFICAR_TODO
+    ; INSERTAR_FINAL 10 EN miLista
+    mov ax, 10
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne GFX_L16
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp GFX_L17
+GFX_L16:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+GFX_L17:
+    call GRAFICAR_TODO
+    ; INSERTAR_FINAL 15 EN miLista
+    mov ax, 15
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne GFX_L18
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp GFX_L19
+GFX_L18:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+GFX_L19:
+    call GRAFICAR_TODO
+    ; INSERTAR_FINAL 1 EN miLista
+    mov ax, 1
+    mov si, [HEAP_PTR]
+    add word ptr [HEAP_PTR], 4
+    mov HEAP[si], ax
+    mov word ptr HEAP[si+2], 0
+    cmp word ptr [miLista_head], 0
+    jne GFX_L20
+    mov [miLista_head], si
+    mov [miLista_tail], si
+    jmp GFX_L21
+GFX_L20:
+    mov bx, [miLista_tail]
+    mov HEAP[bx+2], si
+    mov [miLista_tail], si
+GFX_L21:
+    call GRAFICAR_TODO
+    ; Operacion grafica pendiente: TAMANO miLista  -> T9
+    ; MOSTRAR T9 en modo grafico
+    mov ax, [T9]
     call PRINT_NUM_GRAFICO
-    ; MOSTRAR 50 en modo grafico
-    mov ax, 50
-    call PRINT_NUM_GRAFICO
-    ; MOSTRAR "Código Intermedio OK" en modo grafico
-    mov ax, ["Código Intermedio OK"]
+    ; MOSTRAR 999 en modo grafico
+    mov ax, 999
     call PRINT_NUM_GRAFICO
     call GRAFICAR_TODO
     mov ah, 00h
@@ -175,7 +398,104 @@ PRINT_NUM_GRAFICO endp
 
 GRAFICAR_TODO proc
     call LIMPIAR_PANTALLA
+    call GRAFICAR_PILA_miPila
+    call GRAFICAR_COLA_miCola
+    call GRAFICAR_LISTA_miLista
     ret
 GRAFICAR_TODO endp
+
+GRAFICAR_PILA_miPila proc
+    mov word ptr [gfx_i], 0
+miPila_gp_loop:
+    mov ax, [gfx_i]
+    cmp ax, [miPila_top]
+    jge miPila_gp_fin
+    mov bx, ax
+    shl bx, 1
+    mov ax, miPila[bx]
+    mov [gfx_valor], ax
+    mov ax, [gfx_i]
+    mov bx, 12
+    mul bx
+    mov dx, 180
+    sub dx, ax
+    mov cx, 10
+    mov si, 42
+    mov di, 10
+    mov al, 0Ah
+    call DIBUJAR_RECTANGULO
+    mov cx, 22
+    call SET_CURSOR_PIXEL
+    mov ax, [gfx_valor]
+    call PRINT_NUM_GRAFICO
+    inc word ptr [gfx_i]
+    jmp miPila_gp_loop
+miPila_gp_fin:
+    ret
+GRAFICAR_PILA_miPila endp
+
+GRAFICAR_COLA_miCola proc
+    mov word ptr [gfx_i], 0
+miCola_gc_loop:
+    mov ax, [gfx_i]
+    cmp ax, [miCola_count]
+    jge miCola_gc_fin
+    mov bx, [miCola_front]
+    add bx, ax
+    cmp bx, 50
+    jl miCola_gc_idx_ok
+    sub bx, 50
+miCola_gc_idx_ok:
+    shl bx, 1
+    mov ax, miCola[bx]
+    mov [gfx_valor], ax
+    mov ax, [gfx_i]
+    mov bx, 38
+    mul bx
+    mov cx, 70
+    add cx, ax
+    mov dx, 20
+    mov si, 34
+    mov di, 12
+    mov al, 0Bh
+    call DIBUJAR_RECTANGULO
+    call SET_CURSOR_PIXEL
+    mov ax, [gfx_valor]
+    call PRINT_NUM_GRAFICO
+    inc word ptr [gfx_i]
+    jmp miCola_gc_loop
+miCola_gc_fin:
+    ret
+GRAFICAR_COLA_miCola endp
+
+GRAFICAR_LISTA_miLista proc
+    mov bx, [miLista_head]
+    mov word ptr [gfx_i], 0
+miLista_gl_loop:
+    cmp bx, 0
+    je miLista_gl_fin
+    mov ax, HEAP[bx]
+    mov [gfx_valor], ax
+    push bx
+    mov ax, [gfx_i]
+    mov bx, 42
+    mul bx
+    mov cx, 10
+    add cx, ax
+    mov dx, 48
+    mov si, 34
+    mov di, 12
+    mov al, 0Eh
+    call DIBUJAR_RECTANGULO
+    call SET_CURSOR_PIXEL
+    mov ax, [gfx_valor]
+    call PRINT_NUM_GRAFICO
+    pop bx
+    mov bx, HEAP[bx+2]
+    inc word ptr [gfx_i]
+    jmp miLista_gl_loop
+miLista_gl_fin:
+    ret
+GRAFICAR_LISTA_miLista endp
 
 end main
